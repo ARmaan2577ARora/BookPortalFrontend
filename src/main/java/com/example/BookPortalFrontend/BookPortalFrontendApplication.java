c4daef40587a38fd9eeb4c5a7b49c7b954751d28
Dikshit Singla <110338628+Dikshit2222222@users.noreply.github.com>
2026-05-20 22:18:30 +0530

    package-name-updated

--- src/main/java/com/example/BookPortalFrontend/BookPortalFrontendApplication.java
+++ src/main/java/com/example/BookPortalFrontend/BookPortalFrontendApplication.java
@@ -1,4 +1,4 @@
-package com.example.bookPortalFrontend;
+package com.example.BookPortalFrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
