10db82f9be1415bca2486acea21b2007842eaff1
Ankush <ankushdhiman0421@gmail.com>
2026-05-20 15:35:14 +0530

    Added

--- src/main/java/com/example/BookPortalFrontend/BookPortalFrontendApplication.java
+++ src/main/java/com/example/BookPortalFrontend/BookPortalFrontendApplication.java
@@ -1,13 +1,18 @@
-package com.example.BookPortalFrontend;
+package com.example.bookPortalFrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookPortalFrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookPortalFrontendApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookPortalFrontendApplication.class, args);
-	}
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
